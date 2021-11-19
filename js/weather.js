dcd129afc65de76b286d875f83ee1089742cff9b
Your Name <you@example.com>
2021-11-19 10:45:50 +0800

    Site updated: 2021-11-19 10:45:49

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "0px 0px 0px 0px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "left": "28",
+    "top": "50",
+    "key": "f1d3166e7ff34a629f38c67e8b738bce"
+  }
+}
